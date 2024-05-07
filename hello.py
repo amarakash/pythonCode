5c3fdbb243610a7715eec914c153fbb37443d828
amarakash <106879845+amarakash@users.noreply.github.com>
2024-05-08 00:15:35 +0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+try:
+    import pyautogui as pg
+    import time
+    
+    time.sleep(5)
+    txt=open("animals.txt",'r')
+    
+    n=1
+    for i in txt:
+        pg.write(i)
+        pg.press('Enter')
+        time.sleep(3)
+        n=n+1
+        if(n==5):
+            txt.close()
+            break
+except KeyboardInterrupt:
+    print("\n")
+    txt.close()
